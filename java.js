34b44342aca2ef7f5dc3f1d065f3ba85233b31b4
Vavarooks <vavarooks123@gmail.com>
2020-01-17 15:54:35 -0800

    HTML  edit Java edit

--- java.js
+++ java.js
@@ -1,13 +1,13 @@
-if($_POST["submit"]) {
-    $recipient="babavava420@gmail.com";
-    $subject="Form to email message";
-    $sender=$_POST["sender"];
-    $senderEmail=$_POST["senderEmail"];
-    $message=$_POST["message"];
+// if($_POST["submit"]) {
+//     $recipient="babavava420@gmail.com";
+//     $subject="Form to email message";
+//     $sender=$_POST["sender"];
+//     $senderEmail=$_POST["senderEmail"];
+//     $message=$_POST["message"];
 
-    $mailBody="Name: $sender\nEmail: $senderEmail\n\n$message";
+//     $mailBody="Name: $sender\nEmail: $senderEmail\n\n$message";
 
-    mail($recipient, $subject, $mailBody, "From: $sender <$senderEmail>");
+//     mail($recipient, $subject, $mailBody, "From: $sender <$senderEmail>");
 
-    $thankYou="<p>Thank you! Your message has been sent.</p>";
-}
+//     $thankYou="<p>Thank you! Your message has been sent.</p>";
+// }
