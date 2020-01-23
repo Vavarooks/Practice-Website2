250aaca20968aeaf77b75d65bc7ec9161ac4c10e
Vavarooks <vavarooks123@gmail.com>
2020-01-22 21:09:41 -0800

    Html and Java edits

--- java.js
+++ java.js
@@ -18,3 +18,21 @@ form.onsubmit = function(e) {
     e.preventDefault();
 console.log(form.name.value)
 };
+
+var imageContainer = document.querySelector(".img-container");
+
+imageContainer.addEventListener("click", function(event) {
+  var element = event.target;
+
+  if (element.matches("img")) {
+    var state = element.getAttribute("data-state");
+
+    if (state === "still") {
+      element.setAttribute("data-state", "animate");
+      element.setAttribute("src", element.getAttribute("data-animate"));
+    } else if (state === "animate") {
+      element.setAttribute("data-state", "still");
+      element.setAttribute("src", element.getAttribute("data-still"));
+    }
+  }
+});
